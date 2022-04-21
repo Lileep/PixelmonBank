d857a651ea58602abbe34f026c9e85e5ef0eb80e
administrator <lileep@yeah.net>
2022-04-21 21:37:50 +0800

    feat(del all): Add delete all command.

---
+++ src/main/java/com/github/lileep/pixelmonbank/command/DeleteAllCmd.java
@@ -0,0 +1,43 @@
+package com.github.lileep.pixelmonbank.command;
+
+import com.envyful.api.command.annotate.Child;
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.Argument;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Completable;
+import com.envyful.api.command.annotate.executor.Sender;
+import com.envyful.api.forge.command.completion.player.PlayerTabCompleter;
+import com.envyful.api.player.EnvyPlayer;
+import com.github.lileep.pixelmonbank.PixelmonBank;
+import com.github.lileep.pixelmonbank.handler.SyncHandler;
+import com.github.lileep.pixelmonbank.lib.PermNodeReference;
+import com.pixelmonmod.pixelmon.comm.CommandChatHandler;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextFormatting;
+
+import java.util.Optional;
+
+@Command(
+        value = "deleteall",
+        aliases = {
+                "delall"
+        }
+)
+@Permissible(PermNodeReference.DELETE_NODE)
+@Child
+public class DeleteAllCmd {
+
+    @CommandProcessor
+    public void run(@Sender ICommandSender sender,  @Completable(PlayerTabCompleter.class) @Argument EntityPlayerMP target) {
+        EnvyPlayer<EntityPlayerMP> targetPlayer = PixelmonBank.instance.getPlayerManager().getPlayer(target);
+        if (!Optional.ofNullable(targetPlayer).isPresent()) {
+            return;
+        }
+        if (SyncHandler.getInstance().delAll(targetPlayer.getUuid().toString())){
+            CommandChatHandler.sendFormattedChat(sender, TextFormatting.GREEN, "Reload successful!");
+        }
+
+    }
+}
