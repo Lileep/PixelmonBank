93e8b9007bbb6e819b8cf5b81b923424bea12947
administrator <lileep@yeah.net>
2022-04-22 13:42:23 +0800

    style(Reference): Move Serializer name to main Reference.

--- src/main/java/com/github/lileep/pixelmonbank/lib/Reference.java
+++ src/main/java/com/github/lileep/pixelmonbank/lib/Reference.java
@@ -5,8 +5,9 @@ public class Reference {
     public static final String MOD_ID = "pixelmonbank";
 
     public static final String NAME = "Pixelmon Bank";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String DEPENDENCIES = "required-after:pixelmon@[8.3.1,);";
 
+    public static final String PIXELMON_SERIALIZER = "pixelmon";
 
 }
--- src/main/java/com/github/lileep/pixelmonbank/lib/SerializerReference.java
+++
@@ -1,6 +0,0 @@
-package com.github.lileep.pixelmonbank.lib;
-
-public class SerializerReference {
-
-    public static final String PIXELMON_SERIALIZER = "pixelmon";
-}
