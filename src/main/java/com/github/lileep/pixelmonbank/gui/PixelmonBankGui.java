6991654d00069bbc26d84f9a07b67a6df54ca7f1
administrator <lileep@yeah.net>
2022-04-25 11:43:17 +0800

    feat(pixelmons info): Add pixelmon details to Bank gui.

--- src/main/java/com/github/lileep/pixelmonbank/gui/PixelmonBankGui.java
+++ src/main/java/com/github/lileep/pixelmonbank/gui/PixelmonBankGui.java
@@ -14,7 +14,9 @@ import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class PixelmonBankGui {
 
@@ -28,10 +30,21 @@ public class PixelmonBankGui {
 
         for (Pokemon pokemon : pokemonList) {
             pane.add(GuiFactory.displayableBuilder(ItemStack.class)
-                    .itemStack(new ItemBuilder(UtilSprite.getPixelmonSprite(pokemon)).build())
-                    .clickHandler((envyPlayer, clickType) -> UtilForgeConcurrency.runSync(()-> {
+                    .itemStack(new ItemBuilder(UtilSprite.getPixelmonSprite(pokemon))
+                            .name(UtilChatColour.translateColourCodes('&', "&b" + pokemon.getLocalizedName() + (pokemon.isShiny() ? "&e★" : "")))
+                            .addLore(
+                                    UtilChatColour.translateColourCodes('&', "&dlv: " + pokemon.getLevel() + " | dynamax lv:" + pokemon.getDynamaxLevel() + " " + (pokemon.hasGigantamaxFactor() ? " | can gigantamax" : "")),
+                                    UtilChatColour.translateColourCodes('&', "&dgender: " + pokemon.getGender().getLocalizedName() + " | caught ball: " + pokemon.getCaughtBall().getLocalizedName()),
+                                    UtilChatColour.translateColourCodes('&', (pokemon.getAbilitySlot() == 2 ? "&6" : "&d") + pokemon.getAbility().getLocalizedName() + "&d | " + pokemon.getNature().getLocalizedName() + (Optional.ofNullable(pokemon.getMintNature()).isPresent() ? (" -> &6" + pokemon.getMintNature().getLocalizedName()) : "")),
+                                    UtilChatColour.translateColourCodes('&', "&dHeld " + pokemon.getHeldItem().getDisplayName()),
+                                    UtilChatColour.translateColourCodes('&', "&div: " + MsgHandler.formatStatusValue(pokemon.getIVs().getArray(), '|')),
+                                    UtilChatColour.translateColourCodes('&', "&dev: " + MsgHandler.formatStatusValue(pokemon.getEVs().getArray(), '|')),
+                                    UtilChatColour.translateColourCodes('&', "&dmoves: " + MsgHandler.formatMoves(pokemon.getMoveset().attacks, '|'))
+                            )
+                            .build()
+                    ).clickHandler((envyPlayer, clickType) -> UtilForgeConcurrency.runSync(() -> {
                         envyPlayer.executeCommands("pixelmonbank get " + pokemon.getUUID().toString());
-                        (((EnvyPlayer<EntityPlayerMP>)envyPlayer).getParent()).closeScreen();
+                        (((EnvyPlayer<EntityPlayerMP>) envyPlayer).getParent()).closeScreen();
                     }))
                     .build());
         }
@@ -39,7 +52,7 @@ public class PixelmonBankGui {
         GuiFactory.guiBuilder()
                 .addPane(pane)
                 .height(6)
-                .title(UtilChatColour.translateColourCodes('&',PixelmonBankLocaleConfig.title))
+                .title(UtilChatColour.translateColourCodes('&', PixelmonBankLocaleConfig.title))
                 .setPlayerManager(PixelmonBank.instance.getPlayerManager())
                 .setCloseConsumer(envyPlayer -> {
                 })
--- src/main/java/com/github/lileep/pixelmonbank/handler/MsgHandler.java
+++ src/main/java/com/github/lileep/pixelmonbank/handler/MsgHandler.java
@@ -2,7 +2,10 @@ package com.github.lileep.pixelmonbank.handler;
 
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.github.lileep.pixelmonbank.config.PixelmonBankLocaleConfig;
+import com.pixelmonmod.pixelmon.battles.attacks.Attack;
 import net.minecraft.util.text.TextComponentString;
+
+import java.util.Optional;
 
 public class MsgHandler {
 
@@ -18,16 +21,66 @@ public class MsgHandler {
     }
 
     /**
-     *
      * Add prefix to the given msg,
      * translate color char to real color,
      * process the args
      *
-     * @param msg Message to process
+     * @param msg  Message to process
      * @param args arguments
      * @return processed message
      */
     public static TextComponentString prefixedColorMsg(String msg, Object... args) {
         return prefixedColorMsg(String.format(msg, args));
+    }
+
+    /**
+     * Format an array to a separated string
+     *
+     * @param array     Array that needs to be formatted
+     * @param separator Array separator
+     * @return Formatted string
+     */
+    public static String formatStatusValue(int[] array, char separator) {
+        if (!Optional.ofNullable(array).isPresent()) {
+            return "(!)";
+        }
+        int iMax = array.length - 1;
+        if (iMax == -1) {
+            return "";
+        }
+
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; ; i++) {
+            b.append(array[i]);
+            if (i == iMax) {
+                return b.toString();
+            }
+            b.append(' ').append(separator).append(' ');
+        }
+    }
+
+    /**
+     * Format a move set to a separated string
+     *
+     * @param attacks   Move's attack array that needs to be formatted
+     * @param separator Array separator
+     * @return Formatted move names
+     */
+    public static String formatMoves(Attack[] attacks, char separator) {
+        if (!Optional.ofNullable(attacks).isPresent()) {
+            return "(!)";
+        }
+        int iMax = attacks.length - 1;
+        if (iMax == -1) {
+            return "";
+        }
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; ; i++) {
+            b.append(attacks[i].savedAttack.getLocalizedName());
+            if (i == iMax) {
+                return b.toString();
+            }
+            b.append(' ').append(separator).append(' ');
+        }
     }
 }
