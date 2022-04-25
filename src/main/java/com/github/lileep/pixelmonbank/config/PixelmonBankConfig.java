d399aaf8324cb878433ebf3db65817a32728cb6a
administrator <lileep@yeah.net>
2022-04-25 12:03:29 +0800

    feat(config): Items & Moves black list supported.

--- src/main/java/com/github/lileep/pixelmonbank/config/PixelmonBankConfig.java
+++ src/main/java/com/github/lileep/pixelmonbank/config/PixelmonBankConfig.java
@@ -11,6 +11,8 @@ public class PixelmonBankConfig {
     public static boolean ALLOW_LEGENDARY = true;
     public static boolean ALLOW_ULTRABEAST = true;
     public static String[] BLACK_LIST = new String[0];
+    public static String[] BLACK_LIST_ITEM = new String[0];
+    public static String[] BLACK_LIST_MOVE = new String[0];
     public static int MAX_IVS = 6;
     public static boolean COUNT_HYPER_TRAINED = false;
 
@@ -57,6 +59,14 @@ public class PixelmonBankConfig {
         for (int i = 0; i < BLACK_LIST.length; i++) {
             BLACK_LIST[i] = BLACK_LIST[i].toLowerCase();
         }
+        BLACK_LIST_ITEM = config.get(category, "black_list_item", BLACK_LIST_ITEM, PixelmonBankLocaleConfig.cfgBlackListItem).getStringList();
+        for (int i = 0; i < BLACK_LIST_ITEM.length; i++) {
+            BLACK_LIST_ITEM[i] = BLACK_LIST_ITEM[i].toLowerCase();
+        }
+        BLACK_LIST_MOVE = config.get(category, "black_list_move", BLACK_LIST_MOVE, PixelmonBankLocaleConfig.cfgBlackListMove).getStringList();
+        for (int i = 0; i < BLACK_LIST_MOVE.length; i++) {
+            BLACK_LIST_MOVE[i] = BLACK_LIST_MOVE[i].toLowerCase();
+        }
 
         Property maxIvs = config.get(category, "max_ivs", MAX_IVS, PixelmonBankLocaleConfig.cfgMaxIvs);
         if (maxIvs.getInt() > 6) {
@@ -66,6 +76,7 @@ public class PixelmonBankConfig {
         }
         MAX_IVS = maxIvs.getInt();
         COUNT_HYPER_TRAINED = config.get(category, "count_hyper_trained", COUNT_HYPER_TRAINED, PixelmonBankLocaleConfig.cfgCountHyperTrained).getBoolean();
+
 
 
         category = "database";
