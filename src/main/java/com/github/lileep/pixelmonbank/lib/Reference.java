a69c20c8d2ec9fa66d18421542c4080b3704bd32
Lileep <lileep@yeah.net>
2023-07-30 10:16:02 +0800

    feat(1.16): change the version number and required pixelmon version

--- src/main/java/com/github/lileep/pixelmonbank/lib/Reference.java
+++ src/main/java/com/github/lileep/pixelmonbank/lib/Reference.java
@@ -5,8 +5,8 @@ public class Reference {
     public static final String MOD_ID = "pixelmonbank";
 
     public static final String NAME = "Pixelmon Bank";
-    public static final String VERSION = "1.4.1";
-    public static final String DEPENDENCIES = "required-after:pixelmon@[8.3.1,);";
+    public static final String VERSION = "2.0.0";
+    public static final String DEPENDENCIES = "required-after:pixelmon@[9.1.5,);";
 
     public static final String PIXELMON_SERIALIZER = "pixelmon";
 
